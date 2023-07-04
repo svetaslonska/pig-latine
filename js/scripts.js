e2e18f369a492866fe0abce76c1e3a629d89c243
Svitlana Slonska <svetaslonska@gmail.com>
2023-07-03 17:08:36 -0700

    update

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Pig Latin translation program
+// Author: Svitlana Slonska
+// Date: 07.03.2023
+
+//Creating function to translate text to Pig Latine
+function translatePigLatine(str) {
+    //Creating array with vowels
+    let vowel = ['a', 'e', 'i', 'o', 'u'];
+    //Creating variable result
+    result = str.splite("");
+
+    if (vowels.includes(str.charAt(0))) {
+        return str += "way";
+    }
+}
+
+console.log(translatePigLatine("eight"));
